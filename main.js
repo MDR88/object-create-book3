95ed135771722b5505fac698bf4f69e7b56580fe
mdroberts74 <mikedroberts88@gmail.com>
2018-06-12 12:11:08 -0500

    Added a function to the object and the object.create to the financeAdv object.

--- main.js
+++ main.js
@@ -1,11 +1,77 @@
-const veryImportantInfo = Object.create(null, {
-    socialSecurity: {
-        value: "934-11-0201"
+// Practice
+// Your job is to create an object that represents a financial advisor and has the following properties and methods.
+
+
+
+
+
+// When you invoke the worth() method, it should look at every transaction and calculate the advisor 's net worth.
+
+
+// Company(enumerable, writable, property)
+const financeAdv = Object.create({}, {
+    company: {
+        value: "Jackson Holdings Inc.",
+        enumerable: true,
+        writable: true
+
     },
-    bankAccountNumber: {
-        value: "4483271255"
+    // Specialty(enumerable, writable, property)
+    specialty: {
+        value: "Stock trading",
+        enumerable: true,
+        writable: true
     },
-    bankRoutingNumber: {
-        value: "458979043"
+    // Name(enumerable, property)
+    name: {
+        value: "Fred Rogers",
+        enumerable: true,
+        writable: true
+    },
+    // Portfolio(non - enumerable, property) - Should display the stocks the advisor currently holds
+    portfolio: {
+        value: [],
+        enumerable: true,
+        writable: true
+    },
+    // Worth(non - enumerable, method)
+    worth: {
+        value: "900,000",
+        enumerable: true,
+        writable: true
+    },
+
+    // Purchase(non - enumerable, method) - This method takes a stock ticker symbol, a quantity, and a price as arguments
+
+    purchase: {
+        value: function(stockTicker, qty, price) {
+            let ticker = {
+                name: stockTicker,
+                amount: qty,
+                price
+            }
+            financeAdv.portfolio.push(ticker);
+            console.log(financeAdv.portfolio)
+        }
+    },
+    // Sell(non - enumerable, method) - This method takes a stock ticker symbol, a quantity, and a price as arguments
+
+    sell: {
+        value: function(stockTicker, qty, price, sell) {
+            let ticker = {
+                    name: stockTicker,
+                    amount: qty,
+                    price,
+                    sell
+                },
+
+
+        }
+
     }
-})
+});
+
+console.log(financeAdv)
+
+financeAdv.purchase("bud", 50, 100)
+    // When sell() or purchase() are invoked, then the stock portfolio should be modified accordingly.Start off with making portfolio property an array that holds transactions.
