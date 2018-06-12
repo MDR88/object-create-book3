b515f3cee029477f3ee67340c87c48a2806dcdeb
mdroberts74 <mikedroberts88@gmail.com>
2018-06-12 09:55:30 -0500

    Added an object to the JS file.

---
+++ main.js
@@ -0,0 +1,11 @@
+const veryImportantInfo = Object.create(null, {
+    socialSecurity: {
+        value: "934-11-0201"
+    },
+    bankAccountNumber: {
+        value: "4483271255"
+    },
+    bankRoutingNumber: {
+        value: "458979043"
+    }
+})
